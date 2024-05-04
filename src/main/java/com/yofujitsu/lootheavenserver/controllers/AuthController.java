0ff3c8c8348fd5a660c06f417b7bf6546ea416a7
yofujtisu <yofujitsuuu@gmail.com>
2024-05-04 12:57:44 +0300

    added logout, need to do authority requests for security

--- src/main/java/com/yofujitsu/lootheavenserver/controllers/AuthController.java
+++ src/main/java/com/yofujitsu/lootheavenserver/controllers/AuthController.java
@@ -1,10 +1,22 @@
 package com.yofujitsu.lootheavenserver.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+    @Autowired
+    private OAuth2AuthorizedClientService clientService;
 
+    @GetMapping("/logout")
+    public ResponseEntity<?> logout(OAuth2AuthenticationToken authentication) {
+        if (authentication != null) {
+            clientService.removeAuthorizedClient(authentication.getAuthorizedClientRegistrationId(), authentication.getName());
+        }
+        return ResponseEntity.ok().body("Logged out successfully");
+    }
 }
